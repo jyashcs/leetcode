056fc7377b39f1e88c20b670db6b846323665e06
jyashcs <238r1a6789@gmail.com>
2024-02-15 17:05:01 +0530

    changes

--- First & Second Max.cpp
+++ First & Second Max.cpp
@@ -1,3 +1,14 @@
+/*Find the 1st and second maximum numbers in a given arary.
+
+Example 1 :
+INPUT ARRAY : [2,4,6,7,8,3]
+OUTPUT : 8  7
+
+Example 2 :
+INPUT ARRAY :[9,45,45,23,19]
+OUTPUT : 45 23  */
+
+
 #include <iostream>
 using namespace std;
 int main()
@@ -5,8 +16,8 @@ int main()
         int arr[] = {12, 5, 10, 7, 8, 12};
         int Max = arr[0], SecMax;
         for (int i = 1; i < 6; i++)
-                arr[i] > Max ? SecMax = Max, Max = arr[i] : arr[i] > SecMax && arr[i] != Max ? SecMax = arr[i] : SecMax;
-                                                                                                               
+                arr[i] > Max ? SecMax = Max, Max = arr[i] : arr[i] > SecMax && arr[i] != Max ? SecMax = arr[i]
+                                                                                             : SecMax;
         cout << Max << "\t" << SecMax;
         return 0;
 }
