b2b344eb0b2a7eeb5d5de091a3ac8311bb855b5f
jyashcs <238r1a6789@gmail.com>
2024-04-17 11:40:19 +0530

    modify readme

--- linkedlist2.cpp
+++ linkedlist2.cpp
@@ -1,41 +1,42 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-class node{
-    public:
-    int data;
-    node* next;
-    node(int val){
-        int data=val;
-        next = NULL;
-    }
+class node {
+public:
+  int data;
+  node *next;
+  node(int val) {
+    data = val;
+    next = NULL;
+  }
 };
-void insertattail(node* &head,int val){
-    node *n = new node(val);
-    if(head==NULL){
-        head=n;
-        return;
-    }
-    node *temp =head;
-    while(temp->next!=NULL){
-        temp=temp->next;
-    }
-    temp->next=n;
+void insertattail(node *&head, int val) {
+  node *n = new node(val);
+  if (head == NULL) {
+    head = n;
+    return;
+  }
+  node *temp = head;
+  while (temp->next != NULL) {
+    temp = temp->next;
+  }
+  temp->next = n;
 }
 
-void display(node* head){
-    node* temp = head;
-    while(temp!=NULL){
-        cout<<temp->data<<" ";
-        temp=temp->next;
-    }
-    cout<<endl;
+void display(node *head) {
+  node *temp = head;
+  while (temp != NULL) {
+    cout << temp->data << "->";
+    temp = temp->next;
+  }
+  cout << "NULL" << endl;
 }
-int main(){
-    node* head=NULL;
-    insertattail(head,5);
-    insertattail(head,4);
-    insertattail(head,3);
-    insertattail(head,2);
-    display(head);
-    return 0;
+int main() {
+  node *head = NULL;
+  insertattail(head, 5);
+  insertattail(head, 4);
+  insertattail(head, 3);
+  insertattail(head, 2);
+  insertattail(head, 1);
+  display(head);
+  return 0;
 }
