eb2707621bc6c0d2c10821ebdb1b619224118798
jyashcs <238r1a6789@gmail.com>
2024-07-24 23:07:56 +0530

    169 leetcode Day 1

---
+++ 169 Majority Element.cpp
@@ -0,0 +1,20 @@
+
+class Solution {
+public:
+  int majorityElement(vector<int> &nums) {
+    std::unordered_map<int, int> countMap;
+    int majorityElement = 0;
+    int n = nums.size();
+
+    for (int num : nums) {
+      countMap[num]++;
+
+      if (countMap[num] > n / 2) {
+        majorityElement = num;
+        break;
+      }
+    }
+
+    return majorityElement;
+  }
+};
