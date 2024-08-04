064d6521b2a0c0ea7174ab9b4ee227e41434c591
jyashcs <238r1a6789@gmail.com>
2024-08-05 00:40:43 +0530

    Day 12 Trappint Rain Water

---
+++ Day 12 Trappint Rain Water.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int i = 0, left_max = height[0], sum = 0;
+        int j = height.size() - 1, right_max = height[j];
+        while (i < j) {
+            if (left_max <= right_max) {
+                sum += (left_max - height[i++]);
+                left_max = std::max(left_max, height[i]);
+            } else {
+                sum += (right_max - height[j--]);
+                right_max = std::max(right_max, height[j]);
+            }
+        }
+        return sum;
+    }
+};
